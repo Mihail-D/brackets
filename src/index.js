2beea2e08ee0682f3a47a76d7c4af1e788a0421a
Mihail-D <nuby.4register@gmail.com>
2020-03-22 19:48:21 +0300

    add: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str,bracketsConfig)
+{
+    newArr = str.split('');
+    for (var i = 0; i < newArr.length; i++)
+    {
+        for (var k = 0; k < bracketsConfig.length; k++)
+        {
+            if (bracketsConfig[k][0] == newArr[i] && bracketsConfig[k][1] == newArr[i + 1])
+            {
+                newArr.splice(i,2);
+                i--;
+                k = -1;
+            }
+        }
+    }
+    if (newArr.length == 0)
+    {
+        return true;
+    } else
+    {
+        return false;
+    }
 }
